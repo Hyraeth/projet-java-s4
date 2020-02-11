ea6e39f4348a52982da8bcdd7b27bf381b73c609
Hyraeth <williamkhieu@gmail.com>
2020-02-11 17:00:45 +0100

    Un peu mieux

---
+++ assets/src/main/java/Aquavias/assets/App.java
@@ -0,0 +1,13 @@
+package Aquavias.assets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
