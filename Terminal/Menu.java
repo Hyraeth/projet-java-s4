5c9f40101cf4d4d9cc7a00fb385121a7a5981da1
Julien <julien_li2000@hotmail.com>
2020-02-13 23:43:30 +0100

    Upload New File la où se trouve les fonctions sur le menu.

---
+++ Terminal/Menu.java
@@ -0,0 +1,17 @@
+public class Menu{
+
+  //Constructeur
+  public Menu(){
+
+  }
+
+  //Affiche la barre de menu
+  public void afficherMenu(){
+    System.out.println(
+    "\n              MENU         \n"
+    +  "      _____________________\n\n"
+    +  "            JOUER(j)\n"
+    +  "            CREER(c)\n"
+    +  "           QUITTER(q) \n");
+  }
+}
