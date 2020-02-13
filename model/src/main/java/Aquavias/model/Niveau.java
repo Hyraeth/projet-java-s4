1aa52b6136ee31e35a28dd3f46cea4928eeedd78
Hyraeth <williamkhieu@gmail.com>
2020-02-13 12:47:35 +0100

    ajout de methodes

---
+++ model/src/main/java/Aquavias/model/Niveau.java
@@ -0,0 +1,50 @@
+package Aquavias.model;
+
+public class Niveau {
+    private Tuyau[][] niveau;
+    private int coups;
+    private int score;
+
+    public Niveau() {
+
+    }
+
+    //tourne le tuyau à la position i,j
+    public void rotate(int i, int j) {
+
+    }
+
+    //Calcule l'écoulement de l'eau
+    public void flow() {
+
+    }
+
+    //Importe un niveau
+    public void load() {
+
+    }
+
+    //affiche le plateau dans le terminal
+    public void affiche() {
+        for (int i = 0; i < niveau.length; i++) {
+            for (int j = 0; j < niveau[i].length; j++) {
+                if(niveau[i][j] != null) System.out.print(niveau[i][j]);
+                else System.out.println(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[] getPosRacine() {
+        int tab[] = new int[2];
+        for (int i = 0; i < niveau.length; i++) {
+            for (int j = 0; j < niveau[i].length; j++) {
+                if(niveau[i][j].isRacine()) {
+                    tab[0] = i;
+                    tab[1] = j;
+                }
+            }
+        }
+        return tab;
+    }
+}
--- model/src/main/java/Aquavias/model/Tuyau.java
+++ model/src/main/java/Aquavias/model/Tuyau.java
@@ -1,15 +1,20 @@
 package Aquavias.model;
 
 public class Tuyau {
+    //liste des entrées possibles
     private boolean[] connections = new boolean[4];
+    //savoir si un tuyau est rempli
     private boolean rempli;
-    private char[] afficheTerm;
+    //affiche un char associé à un tuyau
+    private char[] afficheTerm = new char[4];
+    //pour faciliter l'interface gaphique
     private String fileName;
 
     public Tuyau(int i, boolean fill) {
         //chaque i correspond à un tuyau diffrent. fill indique si le tuyau est rempli dès sa création
         switch (i) {
             case 0:
+
                 break;
             case 1:
 
@@ -21,7 +26,7 @@ public class Tuyau {
             
                 break;
             default:
-            System.out.println("Build failed");
+                //Niveaux null
                 break;
         }
     }
@@ -30,6 +35,13 @@ public class Tuyau {
         for (int i = 0; i < connections.length; i++) {
             //On décale le tableau vers la droite
             connections[i] = connections[(i-1)%4];
+            afficheTerm[i] = afficheTerm[(i-1)%4];
+        }
+    }
+
+    public void rotate(int i) {
+        for (int j = 0; j < i; j++) {
+            this.rotate();
         }
     }
 
@@ -42,6 +54,17 @@ public class Tuyau {
             //On regarde si les connections opposé sont true
             if(this.connections[i] && t.connections[(i+2)%4]) return true;
         }
+        //On retourne false si aucune connections n'est possible
+        return false;
+    }
+
+    public String toString() {
+        return afficheTerm[0]+"";
+    }
+
+    //
+    public boolean isRacine() {
+        if(rempli && fileName.equals("racine")) return true;
         return false;
     }
 }
