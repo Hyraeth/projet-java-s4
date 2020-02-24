648246a432268d501f6207775c1a75a6c72278e4
Julien <julien_li2000@hotmail.com>
2020-02-24 15:54:46 +0100

    Replace Test.java

--- Terminal/Test.java
+++ Terminal/Test.java
@@ -2,9 +2,9 @@ public class Test{
   public static void main(String[] args) {
 
     Menu m = new Menu();
-    m.afficherMenu();
+    m.affiche();
 
-    Niveau n = new Niveau();
-    n.afficherNiveau(25);
+    Niveau n = new Niveau(25);
+    n.affiche();
   }
 }
