a77569a57989bf4d331e8f0a85a24d59eafeb85c
Hyraeth <williamkhieu@gmail.com>
2020-03-06 21:12:51 +0100

    useless import

--- Projet/src/main/java/Aquavias/vue/GUI/JPanelPipe.java
+++ Projet/src/main/java/Aquavias/vue/GUI/JPanelPipe.java
@@ -3,7 +3,6 @@ package Aquavias.vue.GUI;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.awt.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
