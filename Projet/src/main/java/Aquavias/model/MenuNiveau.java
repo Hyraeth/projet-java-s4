559e375a220de89640633e02974c65ca02026bcc
Julien <julien_li2000@hotmail.com>
2020-03-10 18:15:16 +0100

    Correction affichage.

--- Projet/src/main/java/Aquavias/model/MenuNiveau.java
+++ Projet/src/main/java/Aquavias/model/MenuNiveau.java
@@ -1,8 +1,18 @@
 package Aquavias.model;
 
+import Aquavias.vue.GUI.VueTerm;
+
 public class MenuNiveau{
-  public int Norm; //Nombre de niveau normaux
-  public int Pers; //Nombre de niveau personnalisés
+  private int Norm; //Nombre de niveau normaux
+  private int Pers; //Nombre de niveau personnalisés
+  private VueTerm vt = new VueTerm();
+
+  public int getNorm(){
+    return Norm;
+  }
+  public int getPers(){
+    return Pers;
+  }
 
   //Constructeur
   public MenuNiveau(int n,int p){
@@ -19,70 +29,13 @@ public class MenuNiveau{
     Generation g1 = Generation.generer(i,j);
     Niveau n1 = new Niveau(i,j);
     n1.setNiveau(Generation.const1(g1.getTab()));
-    n1.affiche();
+    vt.afficheNiv(n1);
   }
 
   //Permet de lancer le niveau personnalisé numéro x
   public void LanceNivP(int x){
     System.out.println("Voir dans le fichier JSON le niveau personnalisé numero "+ x);
     System.out.println("Disponible dans une prochaîne mise à jour...");
-  }
-
-  //Affiche la liste de tous les niveaux normaux
-  public void afficheNorm(){
-
-    System.out.println(Color.BLUE_BRIGHT
-    +"\n             AQUAVIAS         \n"
-    +  "     ________________________"
-    +Color.RESET);
-    for(int i=0; i<Norm; i++){ //Norm le nombre de niveaux normaux
-
-      if(i%5 == 0){ //Saut de ligne chaque 5 niveaux
-        System.out.println();
-        System.out.print("       ");
-      }
-      if(i == 0) System.out.print(Color.WHITE_BOLD+" "+(1)+" "+Color.RESET);
-      //Il faudra vérifier si le niveau a déjà été complété pour afficher les niveaux avec différentes couleurs
-      else System.out.print(" "+(i+1)+" ");
-      if(i<10) System.out.print(" ");//pour rendre l'affichage plus beau
-    }
-    System.out.println("\n\nRetour(r)  Quitter(q)\n");
-    //Exemple de couleurs qui seront utilisées
-    System.out.println(
-    Color.YELLOW_BOLD+"  Niveau déjà complété. \n"+Color.RESET
-    +Color.WHITE+"  Niveau pas encore complété. \n"+Color.RESET
-    +Color.WHITE_BOLD+"  Niveau à compléter. \n"+Color.RESET);
-
-  }
-
-  //Affiche la liste de tous les niveaux personnalisés
-  public void affichePers(){
-
-    System.out.println(
-    Color.WHITE_BOLD_BRIGHT
-    +"\n \033[3m ~BIENVENUE DANS LES NIVEAUX PERSONALISES~ \033[0m \n"
-    +Color.RESET
-    +Color.BLUE_BRIGHT
-    +"\n             AQUAVIAS         \n"
-    +  "     ________________________"
-    +Color.RESET);
-    for(int i=0; i<Pers; i++){ //pers le nombre de niveaux personnalisés
-
-      if(i%5 == 0){ //Saut de ligne chaque 5 niveaux
-        System.out.println();
-        System.out.print("       ");
-      }
-      if(i == 0) System.out.print(Color.WHITE_BOLD+" "+(1)+" "+Color.RESET);
-      //Il faudra vérifier si le niveau a déjà été complété pour afficher les niveaux avec différentes couleurs
-      else System.out.print(" "+(i+1)+" ");
-      if(i<10) System.out.print(" ");//pour rendre l'affichage plus beau
-    }
-    System.out.println("\n\nRetour(r)  Quitter(q)\n");
-    //Exemple de couleurs qui seront utilisées
-    System.out.println(
-    Color.YELLOW_BOLD+"  Niveau déjà complété. \n"+Color.RESET
-    +Color.WHITE+"  Niveau pas encore complété. \n"+Color.RESET
-    +Color.WHITE_BOLD+"  Niveau à compléter. \n"+Color.RESET);
   }
 
   public void jouer(int lvl, Niveau n) {
@@ -106,9 +59,9 @@ public class MenuNiveau{
             coups++;
           }
           else System.out.println("Vous n'avez pas rentré une valeur correct. Réessayez.");
-          n.affiche();                 // affiche le niveau à chaque fois qu'on tourne un tuyau
+          vt.afficheNiv(n);                 // affiche le niveau à chaque fois qu'on tourne un tuyau
         }
-        Score.afficher(n.getScore(), coups, lvl);  // en attendant de coder json, on fait pour niveau 1
+        vt.afficheScore(n.getScore(), coups, lvl);  // en attendant de coder json, on fait pour niveau 1
       }
 
       private int premier(int str) {
