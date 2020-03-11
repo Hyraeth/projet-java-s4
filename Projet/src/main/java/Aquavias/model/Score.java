d39874c4879f09922ddf525d91184229e8dddaf9
Julien <julien_li2000@hotmail.com>
2020-03-11 11:56:50 +0100

    Replace Score.java

--- Projet/src/main/java/Aquavias/model/Score.java
+++ Projet/src/main/java/Aquavias/model/Score.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Score {
 
-  private static void recommencer(int lvl) {     // recommencer le niveau
+  public static void recommencer(int lvl) {     // recommencer le niveau
     MenuNiveau n = new MenuNiveau(25,13);
     if (lvl<1 || lvl>n.getNorm()) throw new IndexOutOfBoundsException(); //Vérifie si le niveau existe
     else n.LanceNivN(lvl); //Lance le niveau numéro x
   }
 
-  private static void suivant(int lvl) {         // niveau suivant
+  public static void suivant(int lvl) {         // niveau suivant
     MenuNiveau n = new MenuNiveau(25,13);
     if (lvl+1<1 || lvl+1>n.getNorm()) throw new IndexOutOfBoundsException(); //Vérifie si le niveau existe
     else n.LanceNivN(lvl+1); //Lance le niveau numéro x
   }
 
-  private static void retour() {          // retourner à la liste des niveaux
+  public static void retour() {          // retourner à la liste des niveaux
     return;
   }
 
