06d7fc434265d847af9e7f81c954abd1064f6a76
ruslan <ruslandanyshchuk@gmail.com>
2020-03-11 15:00:27 +0100

    ajout score IG

---
+++ Projet/src/main/java/Aquavias/vue/GUI/Test.java
@@ -0,0 +1,13 @@
+package Aquavias.vue.GUI;
+
+public class Test {
+
+  public static void main(String[] args) {
+    int[] compa = new int[3];
+    compa[0] = 20;
+    compa[1] = 15;
+    compa[2] = 10;
+    ScoreIG score = new ScoreIG(compa, 20, 1);
+  }
+
+}
